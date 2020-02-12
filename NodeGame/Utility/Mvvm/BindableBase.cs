01a7517a8c9baaedb7de94db90c3a5c380696eba
RNK <rizanurkuru@gmail.com>
2020-02-12 12:42:57 +0200

    BindableBase.cs added

---
+++ NodeGame/Utility/Mvvm/BindableBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Utility.Mvvm
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
